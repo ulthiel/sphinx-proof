2b61f02ce7ea799f24c2409b41217c7e60e0420c
Ulrich Thiel <thiel@x870.fritz.box>
2026-01-16 10:36:57 +0100

    Fix in Beweis

--- sphinx_proof/domain.py
+++ sphinx_proof/domain.py
@@ -83,7 +83,7 @@ class ProofDomain(Domain):
 
     indices = {ProofIndex}  # a list of index subclasses
 
-    directives = {**{"proof": ProofDirective, BeweisDirective}, **PROOF_TYPES}  # list of directives
+    directives = {**{"proof": ProofDirective}, **{"beweis": BeweisDirective}, **PROOF_TYPES}  # list of directives
 
     enumerable_nodes = {}  # type: Dict[[Node], Tuple[str, Callable]]
 
